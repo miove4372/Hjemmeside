8b2a00578fee7e565526199b946a70ab7f215d46
mikaeov <mikaeov@ntnu.no>
2025-09-17 18:01:41 +0200

    første push

---
+++ src/app/projekter/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/conponements/header";
+
+export default function Projekter(){
+    return (
+        <div>
+            
+            <h1>hei fra Projekter</h1>
+            
+        </div>
+    )
+}
