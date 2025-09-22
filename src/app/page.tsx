c753ad0902c15ca7e9c2c8e4c91bece9e457288f
mikaeov <mikaeov@ntnu.no>
2025-09-22 15:09:38 +0200

    igjen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-white">
       <h1 className="text-2xl text-red-600">hei</h1>
-      <p>jeg heter Mikael</p>
-      <a href="http://localhost:3000/projekter">projekter</a>
-      <a href="https://echo.uib.no/hjem">echo</a>
+      <p className="text-red-600">jeg heter Mikael</p>
+      <a className="text-red-600" href="http://localhost:3000/projekter">
+        projekter
+      </a>
+      <a className="text-red-600" href="https://echo.uib.no/hjem">
+        echo
+      </a>
+      <div className="flex justify-center">
+        <img src="/Snapchat-1031077897.png" className="max-w-64" />
+      </div>
     </div>
   );
 }
