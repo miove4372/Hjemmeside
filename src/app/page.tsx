5bf6165e6fc62bbe62cced291bc9e55c7a498458
mikaeov <mikaeov@ntnu.no>
2025-09-24 18:02:15 +0200

    hei

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-white">
+    <div className="bg-white min-h-screen">
       <h1 className="text-2xl text-red-600">hei</h1>
       <p className="text-red-600">jeg heter Mikael</p>
       <Link className="text-red-600" href="http://localhost:3000/projekter">
@@ -15,6 +15,21 @@ export default function Home() {
       </Link>
       <div className="flex justify-center">
         <img src="/Snapchat-1031077897.png" className="max-w-64" />
+      </div>
+      <div>
+        <h1>Socials</h1>
+        <ul className="text-black">
+          <li>
+            <Link href="https://www.facebook.com/profile.php?id=100006384786377">
+              Facebook
+            </Link>
+          </li>
+          <li>
+            <Link href="https://www.instagram.com/_mikaeloverland/">
+              Instagram
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
