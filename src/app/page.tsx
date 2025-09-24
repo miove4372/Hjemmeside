289388e3c11bb2819ce9802c2be8c9cead86a632
mikaeov <mikaeov@ntnu.no>
2025-09-24 17:41:41 +0200

    hei

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import Header from "@/conponements/header";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="bg-white">
       <h1 className="text-2xl text-red-600">hei</h1>
       <p className="text-red-600">jeg heter Mikael</p>
-      <a className="text-red-600" href="http://localhost:3000/projekter">
+      <Link className="text-red-600" href="http://localhost:3000/projekter">
         projekter
-      </a>
-      <a className="text-red-600" href="https://echo.uib.no/hjem">
+      </Link>
+      <Link className="text-red-600" href="https://echo.uib.no/hjem">
         echo
-      </a>
+      </Link>
       <div className="flex justify-center">
         <img src="/Snapchat-1031077897.png" className="max-w-64" />
       </div>
